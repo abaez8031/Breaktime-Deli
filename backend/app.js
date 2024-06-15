959cecfc4d20f45f28daf27e2a3abd8239c96ad5
abaez8031 <abaez8031@gmail.com>
2024-06-15 01:35:13 -0400

    404 error

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
+const { isProduction } = require("./config/keys");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { isProduction } = require("./config/keys");
 const cors = require("cors");
 const csurf = require("csurf");
-
+const debug = require('debug')
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require("./routes/api/tweets");
 const csrfRouter = require('./routes/api/csrf')
-
 const app = express();
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,8 +27,30 @@ app.use(
     }
   })
 );
+
+// Attach routers
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter)
 
+// Custom middleware for catching all unmatched requests and formatting a 404 error to be sent as the response.
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Custom error handler that will be called when a route handler/middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 module.exports = app;
