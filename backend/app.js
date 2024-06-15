9973c17f40d5294022af3e61fafa5835b16e2075
abaez8031 <abaez8031@gmail.com>
2024-06-15 01:29:02 -0400

    csurf

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { isProduction } = require("./config/keys")
-const cors = require("cors")
+const { isProduction } = require("./config/keys");
+const cors = require("cors");
+const csurf = require("csurf");
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require('./routes/api/csrf')
 
 const app = express();
 
@@ -16,7 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 if (!isProduction) app.use(cors())
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && 'Lax',
+      httpOnly: true
+    }
+  })
+);
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter)
 
 module.exports = app;
