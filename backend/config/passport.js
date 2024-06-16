5e8383032dae5639fbc98f5c08d70d4ba0bcb3c0
abaez8031 <abaez8031@gmail.com>
2024-06-15 21:31:41 -0400

    jwttoken

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const jwt = require("jsonwebtoken")
 const { secretOrKey } = require("./keys")
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt")
 
 passport.use(new LocalStrategy({
   session: false,
@@ -12,16 +13,20 @@ passport.use(new LocalStrategy({
   passwordField: 'password',
 }, async function (username, password, done) {
   const user = await User.findOne({ username });
+  // look for user with the matching username in DB
   if (user) {
+    // if there is a found user, compare the input pw to hash in DB using bcrypt
     bcrypt.compare(password, user.hashedPassword, (err, isMatch) => {
       if (err || !isMatch) done(null, false);
       else done(null, user);
+      // return the user
     });
   } else
     done(null, false);
 }));
 
 exports.loginUser = async(user) => {
+  // create an object with non-sensitive user information to send back
   const userInfo = {
     _id: user._id,
     username: user.username
@@ -36,3 +41,32 @@ exports.loginUser = async(user) => {
     token
   }
 }
+
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = secretOrKey;
+
+passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
+  try {
+    const user = await User.findById(jwtPayload._id)
+    if (user) {
+      // return the user to the frontend
+      return done(null, user);
+    }
+    // return false since there is no user
+    return done(null, false);
+  }
+  catch(err) {
+    done(err);
+  }
+}));
+
+exports.requireUser = passport.authenticate('jwt', { session: false });
+
+exports.restoreUser = (req, res, next) => {
+  return passport.authenticate('jwt', { session: false }, function(err, user) {
+    if (err) return next(err);
+    if (user) req.user = user;
+    next();
+  })(req, res, next);
+};
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,24 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
-const passport = require('passport');
-const { loginUser } = require("../../config/passport")
+const passport = require("passport");
+const { loginUser, restoreUser } = require("../../config/passport");
+const { isProduction } = require("../../config/keys")
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   const { username } = req.body;
-  const user = await User.findOne({ username })
-  if(user) {
+  const user = await User.findOne({ username });
+  if (user) {
     const err = new Error("Validation Error");
     err.statusCode = 400;
     err.errors = {
-      username: "Username already exists"
-    }
-    return next(err)
+      username: "Username already exists",
+    };
+    return next(err);
   }
   const newUser = new User({
-    username
+    username,
   });
 
   bcrypt.genSalt(10, (err, salt) => {
@@ -29,25 +30,36 @@ router.post('/register', async (req, res, next) => {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
         return res.json(await loginUser(user));
-      }
-      catch(err) {
+      } catch (err) {
         next(err);
       }
-    })
+    });
   });
 });
 
-router.post("/login", async(req,res,next) => {
+router.post("/login", async (req, res, next) => {
   passport.authenticate("local", async (err, user) => {
     if (err) return next(err);
-    if(!user) {
+    if (!user) {
       const err = new Error("Invalid Credentials");
       err.statusCode = 400;
-      err.errors = { username: "Invalid Credentials" }
-      return next(err)
+      err.errors = { username: "Invalid Credentials" };
+      return next(err);
     }
-    return res.json(await loginUser(user))
-  })(req,res,next);
+    return res.json(await loginUser(user));
+  })(req, res, next);
+});
+
+router.get("/current", restoreUser, (req,res) => {
+  if (!isProduction) {
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if(!req.user) return res.json(null)
+  res.json({
+    _id: req.user._id,
+    username: req.user.username
+  })
 })
 
 module.exports = router;
