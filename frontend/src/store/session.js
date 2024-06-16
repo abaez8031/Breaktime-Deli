884a1a46a3d9c47492c1f64dadc9f07925afc1a3
abaez8031 <abaez8031@gmail.com>
2024-06-16 03:23:36 -0400

    reducers

---
+++ frontend/src/store/session.js
@@ -0,0 +1,71 @@
+import jwtFetch from "./jwt";
+
+const RECEIVE_USER = "session/RECEIVE_USER";
+const RECEIVE_SESSION_ERRORS = "session/RECEIVE_SESSION_ERRORS";
+const CLEAR_SESSION_ERRORS = "session/CLEAR_SESSION_ERRORS";
+const REMOVE_USER = "session/REMOVE_USER";
+
+const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user
+})
+
+const removeUser = () => ({
+  type: REMOVE_USER
+})
+
+const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+})
+
+const clearErrors = () => ({
+  type: CLEAR_SESSION_ERRORS
+})
+
+const startSession = (userInfo,route) => async dispatch => {
+  try {
+    const res = await jwtFetch(route, {
+      method: "POST",
+      body: JSON.stringify(userInfo)
+    })
+    const { user,token } = await res.json()
+    localStorage.setItem("jwtToken", token)
+    return dispatch(receiveUser(user))
+  } catch(err) {
+    const res = await err.json();
+    if(res.statusCode === 400) {
+      return dispatch(receiveErrors(res.errors))
+    }
+  }
+}
+
+export const signup = user => startSession(user, "api/users/register");
+export const login = user => startSession(user, "api/users/login");
+export const logout = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  dispatch(removeUser())
+}
+
+const initialState = { user: null }
+const sessionReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case RECEIVE_USER:
+      return {...state, user: action.user}
+    case REMOVE_USER:
+      return initialState;
+    default:
+      return state;
+  }
+}
+export const sessionErrorsReducer = (state = null, action) => {
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors
+    case CLEAR_SESSION_ERRORS:
+      return null
+    default:
+      return state
+  }
+}
+export default sessionReducer;
