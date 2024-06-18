fa81c5450e2eb4b27325f64b34070bd354570210
abaez8031 <abaez8031@gmail.com>
2024-06-17 22:30:49 -0400

    bughunt

--- frontend/src/store/jwt.js
+++ frontend/src/store/jwt.js
@@ -1,3 +1,12 @@
+const getCookie = (cookieName) => {
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name.trim() === cookieName) return value;
+  }
+  return null;
+}
+
 const jwtFetch = async (url, options = {}) => {
   // if there is no method set it to 'GET'
   options.method ||= "GET";
@@ -7,23 +16,16 @@ const jwtFetch = async (url, options = {}) => {
   const jwtToken = localStorage.getItem("jwtToken")
   if (jwtToken) options.headers['Authorization'] = "Bearer " + jwtToken
   // if the method is anything other than 'GET' set the Content-Type header to be be application/json
-  if(options.method !== "GET" ) options.headers["Content-Type"] ||= "application/json"
+  if(options.method.toUpperCase() !== "GET" ) {
+    options.headers["Content-Type"] ||= "application/json"
   // set the csurf token header to the CSRF-Token in the cookie 
   options.headers["CSRF-Token"] = getCookie("CSRF-TOKEN");
+}
   // call fetch with the url and the updated options object
   const res = await fetch(url, options)
     // If the response status code is 400 or above, then throw an error with the error being the response.
   if (res.status >= 400) throw res;
   return res;
-}
-
-function getCookie(cookieName) {
-  const cookies = document.cookie.split(';');
-  for (let cookie of cookies) {
-      const [name, value] = cookie.split('=');
-      if (name.trim() === cookieName) return value;
-  }
-  return null;
 }
 
 export default jwtFetch;
