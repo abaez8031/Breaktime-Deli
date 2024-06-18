a7fe6797a27740e944226a82e02bfce6a6612256
abaez8031 <abaez8031@gmail.com>
2024-06-18 19:41:47 -0400

    suggestionroutes

--- backend/routes/api/suggestions.js
+++ backend/routes/api/suggestions.js
@@ -11,3 +11,26 @@ router.get("/", async (req,res,next) => {
   }
 })
 
+router.post("/", async (req,res,next) => {
+  const { text } = req.body;
+  const suggestion = new Suggestion({
+    userId: req.user._id,
+    text
+  })
+  try {
+    const savedSuggestion = await suggestion.save();
+    res.status(201).json(savedSuggestion)
+  } catch(err) {
+    next(err)
+  }
+})
+
+router.delete("/:id", async (req,res,next) => {
+  const { id } = req.params;
+  try {
+    const deletedSuggestion = await Suggestion.findByIdAndDelete(id);
+    res.json(deletedSuggestion);
+  } catch(err) {
+    next(err)
+  }
+})
