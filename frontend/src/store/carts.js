3f498a218015da01b153d7a3b0050e7dcedd476c
abaez8031 <abaez8031@gmail.com>
2024-06-24 03:20:50 -0400

    cartsreducer

--- frontend/src/store/carts.js
+++ frontend/src/store/carts.js
@@ -1,3 +1,30 @@
-const ADD_SANDWICH_TO_CART = "carts/ADD_SANDWICH_TO_CART";
-const REMOVE_SANDWICH_FROM_CART = "carts/REMOVE_SANDWICH_FROM_CART";
+const ADD_TO_CART = "carts/ADD_TO_CART";
+const REMOVE_FROM_CART = "carts/REMOVE_FROM_CART";
 const CLEAR_CART = "carts/CLEAR_CART";
+
+export const addToCart = sandwich => ({
+  type: ADD_TO_CART,
+  sandwich
+});
+
+export const removeFromCart = sandwichId => ({
+  type: REMOVE_FROM_CART,
+  sandwichId
+});
+
+export const clearCart = () => ({
+  type: CLEAR_CART
+})
+
+const cartReducer = (state = {sandwiches: []}, action) => {
+  switch(action.type) {
+    case ADD_TO_CART:
+      return {...state, sandwiches: [...state.sandwiches, action.sandwich]}
+    case REMOVE_FROM_CART:
+      return {...state, sandwiches: state.sandwiches.filter(sandwich => sandwich._id !== action.sandwichId)}
+    case CLEAR_CART:
+      return {sandwiches: []}
+    default:
+      return state;
+  }
+}
