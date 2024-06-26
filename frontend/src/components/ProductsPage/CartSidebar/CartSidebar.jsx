5dc0b047eff1a15432e1e0231cbce1e007fb86f4
abaez8031 <abaez8031@gmail.com>
2024-06-26 16:44:45 -0400

    cartsidebar

--- frontend/src/components/ProductsPage/CartSidebar/CartSidebar.jsx
+++ frontend/src/components/ProductsPage/CartSidebar/CartSidebar.jsx
@@ -1,9 +1,10 @@
 import "./CartSidebar.css"
 
-const CartSidebar = () => {
+const CartSidebar = ({isOpen, onClose}) => {
   return (
     <div className="cart-sidebar-container">
-      
+      <button className="close-sidebar-btn" onClick={onClose}>x</button>
+      <h2 className="cart-sidebar-header">Your Cart</h2>
     </div>
   )
 }
