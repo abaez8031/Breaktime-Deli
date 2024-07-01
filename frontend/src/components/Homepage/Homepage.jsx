ad216e7d3ed6adf4d8e31391c994ddd30943ce37
abaez8031 <abaez8031@gmail.com>
2024-07-01 05:32:01 -0400

    homepage

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -1,11 +1,18 @@
 import "./Homepage.css"
+import { Link } from "react-router-dom"
 
 const Homepage = () => {
   return (
-    <div className="homepage-container">
-      <h1>HELLO FROM THE HOMEPAGE</h1>
+    <div className="homepage">
+
+      <div className="hero">
+        <h1>Welcome to Breaktime Deli</h1>
+        <p>We offer the best sandwiches in town, made with fresh ingredients and lots of love.</p>
+        <Link to="/products">Order Now</Link>
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Homepage;
